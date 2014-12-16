b3e2f8163636690748f39c55a230368dd67e8508
Jason K. Moore <moorepants@gmail.com>
2014-12-16 11:03:56 -0500

    Changed to hamming window add second figure.

--- src/treadmill_motion.py
+++ src/treadmill_motion.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import parzen
+from scipy.signal import hamming
 from scipy.fftpack import fft
 
 import utils
@@ -25,17 +25,25 @@ N = len(time)
 T = 1.0 / 100.0
 
 yf = fft(belt_speed)
-window = parzen(N)
+window = hamming(N)
 ywf = fft(belt_speed * window)
 
 xf = np.linspace(0.0, 1.0 / (2.0 * T), N / 2)
 
-plt.semilogy(xf[1:N / 2], utils.smooth(2.0 / N * np.abs(yf[1:N / 2]), 20), '-b')
-plt.semilogy(xf[1:N / 2], utils.smooth(2.0 / N * np.abs(ywf[1:N / 2]), 20), '-r')
+amp_f = 2.0 / N * np.abs(yf[1:N / 2])
+amp_wf = 2.0 / N * np.abs(ywf[1:N / 2])
 
-plt.xlim((0.0, 8.0))
+fig, ax = plt.subplots()
+ax.semilogy(xf[1:N / 2], amp_f, '-b')
+ax.semilogy(xf[1:N / 2], utils.smooth(amp_wf, 20), '-r')
+ax.set_xlim((0.0, 8.0))
+ax.legend(['FFT', 'FFT Moving Average'])
 
-plt.legend(['FFT', 'FFT w. window'])
+fig, ax = plt.subplots()
+ax.semilogy(xf[1:N / 2], utils.smooth(amp_f, 20), '-b')
+ax.semilogy(xf[1:N / 2], utils.smooth(amp_wf, 20), '-r')
+ax.set_xlim((0.0, 8.0))
+ax.legend(['FFT', 'FFT w. window'])
 
 plt.grid()
 plt.show()
