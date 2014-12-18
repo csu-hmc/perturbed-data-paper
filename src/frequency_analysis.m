8f47d340779e1d11b103cc7128f4258a8ca092e3
Sandra Hnat <skhnat@gmail.com>
2014-12-18 15:48:48 -0500

    addresses #39 and #106

--- src/frequency_analysis.m
+++ src/frequency_analysis.m
@@ -31,18 +31,6 @@ clc
 %-------------------------------------------------------------------------
 %Spectral Analysis
 %-------------------------------------------------------------------------
-    %Settings for Nice Plot Appearance 
-        set(0,'defaulttextinterpreter','latex')
-        set(0,'DefaultAxesFontName', 'latex')
-        ti = get(gca,'TightInset');
-        set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
-        set(gca,'units','centimeters')
-        pos = get(gca,'Position');
-        ti = get(gca,'TightInset');
-        set(gcf, 'PaperUnits','centimeters');
-        set(gcf, 'PaperSize', [pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
-        set(gcf, 'PaperPositionMode', 'manual');
-        set(gcf, 'PaperPosition',[0 0 pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
     %Analysis
         fs=100;
         T=1/fs;
@@ -68,10 +56,10 @@ clc
                 hold on
                 semilogy(f_output,p_output_mean,'Color',[0.815 0.3294 0.3020])
                 ylabel('PSD (v^2/Hz)')
-                xlim([0 8])
+                xlim([0 8]); ylim([10^-5 10^0])
                 title(titles{i})
                 if i==1
-                    legend('Commanded','Measured','Location','southeast')
+                    legend('Commanded','Measured','Location','northeast')
                 end
                 if i==3
                     xlabel('Frequency (Hz)');
