5c46e9a7fcad34605647e5de638a1f92a7881d8e
Sandra Hnat <skhnat@gmail.com>
2014-12-18 16:39:48 -0500

    made adjustments for PDF printing

--- src/signal_check.m
+++ src/signal_check.m
@@ -27,6 +27,16 @@ clc
 %-------------------------------------------------------------------------
 %Plotting
 %-------------------------------------------------------------------------
+    %Settings to Improve Graph Appearance
+        ti = get(gca,'TightInset');
+        set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
+        set(gca,'units','centimeters')
+        pos = get(gca,'Position');
+        ti = get(gca,'TightInset');
+        set(gcf, 'PaperUnits','centimeters');
+        set(gcf, 'PaperSize', [pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
+        set(gcf, 'PaperPositionMode', 'manual');
+        set(gcf, 'PaperPosition',[0 0 pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
     figure(1)
         subplot(3,1,1)
         plot(time_input, inputs(:,1),'Color',[0.815 0.3294 0.3020])
