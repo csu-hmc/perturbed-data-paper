238fb821d47f4912aa38c0be0b5bdf2d6326d70e
Jason K. Moore <moorepants@gmail.com>
2015-03-16 17:09:39 -0700

    Started to update the lateral io plot.

--- src/lateral_perturbation_plot.m
+++ src/lateral_perturbation_plot.m
@@ -1,13 +1,18 @@
 clear
 clc
 
+PATHS = get_config.m;
+
 %-------------------------------------------------------------------------
 %Loading, Parsing, and Truncating
 %-------------------------------------------------------------------------
-    perturbation_input = importdata('Lateral_Perturbation.txt');
-    perturbation_output = importdata('mocap-006.txt');
-    frame=1:1:length(perturbation_input);            
-    time_input=(frame-1)*0.0033;                    
+    perturb_sig_dir = [PATHS.raw_data_dir filesep 'perturbation-signals'];
+    lat_per_file = [perturb_sig_dir filesep 'lateral-perturbation.txt'];
+    perturbation_input = importdata(lat_per_file);
+    mocap_file = [PATHS.raw_data filesep 'T006' filesep 'mocap-006.txt'];
+    perturbation_output = importdata(mocap_file);
+    frame=1:1:length(perturbation_input);
+    time_input = (frame-1)*0.0033;
     signal_input = perturbation_input(:,2);
     time_output = perturbation_output.data(33090:57336,1);
     time_output = time_output - time_output(1,1);
@@ -15,7 +20,7 @@ clc
     signal_output = signal_output - signal_output(1,1);
     signal_output = interp1(time_output,signal_output,time_input);
 %-------------------------------------------------------------------------
-%Plotting 
+%Plotting
 %-------------------------------------------------------------------------
     %Settings to Improve Graph Appearance
         ti = get(gca,'TightInset');
@@ -26,7 +31,7 @@ clc
         set(gcf, 'PaperUnits','centimeters');
         set(gcf, 'PaperSize', [pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
         set(gcf, 'PaperPositionMode', 'manual');
-        set(gcf, 'PaperPosition',[0 0 pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]); 
+        set(gcf, 'PaperPosition',[0 0 pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
     %Plot
         plot(time_input, signal_input,'Color',[0.815 0.3294 0.3020])
         hold on
