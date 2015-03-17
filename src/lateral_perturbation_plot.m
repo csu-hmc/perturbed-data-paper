209dbde527116023d5a5f7a7e6c30f2355a71910
Jason K. Moore <moorepants@gmail.com>
2015-03-17 01:10:05 -0700

    The lateral perturb figure now runs from the Makefile.

--- src/lateral_perturbation_plot.m
+++ src/lateral_perturbation_plot.m
@@ -1,46 +1,70 @@
+% This script generates a plot of the lateral deviation of the treadmill
+% base durig trial number 6.
+
 clear
 clc
 
-PATHS = get_config.m;
+%-------------------------------------------------------------------------
+% File Paths
+%-------------------------------------------------------------------------
+
+PATHS = get_config();
+
+per_sig_dir = [PATHS.raw_data_dir filesep 'perturbation-signals'];
+lat_per_file = [per_sig_dir filesep 'lateral-perturbation.txt'];
+mocap_file = [PATHS.raw_data_dir filesep 'T006' filesep 'mocap-006.txt'];
 
 %-------------------------------------------------------------------------
-%Loading, Parsing, and Truncating
+% Loading, Parsing, and Truncating
 %-------------------------------------------------------------------------
-    perturb_sig_dir = [PATHS.raw_data_dir filesep 'perturbation-signals'];
-    lat_per_file = [perturb_sig_dir filesep 'lateral-perturbation.txt'];
-    perturbation_input = importdata(lat_per_file);
-    mocap_file = [PATHS.raw_data filesep 'T006' filesep 'mocap-006.txt'];
-    perturbation_output = importdata(mocap_file);
-    frame=1:1:length(perturbation_input);
-    time_input = (frame-1)*0.0033;
-    signal_input = perturbation_input(:,2);
-    time_output = perturbation_output.data(33090:57336,1);
-    time_output = time_output - time_output(1,1);
-    signal_output = perturbation_output.data(33090:57336,3);
-    signal_output = signal_output - signal_output(1,1);
-    signal_output = interp1(time_output,signal_output,time_input);
+
+perturbation_input = importdata(lat_per_file);
+frame = 1:1:length(perturbation_input);
+time_input = (frame - 1) * 0.0033;
+signal_input = perturbation_input(:, 2);
+
+perturbation_output = importdata(mocap_file);
+time_output = perturbation_output.data(33090:57336, 1);
+time_output = time_output - time_output(1, 1);
+signal_output = perturbation_output.data(33090:57336, 3);
+signal_output = signal_output - signal_output(1, 1);
+signal_output = interp1(time_output,signal_output,time_input);
+
 %-------------------------------------------------------------------------
-%Plotting
+% Plotting
 %-------------------------------------------------------------------------
-    %Settings to Improve Graph Appearance
-        ti = get(gca,'TightInset');
-        set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
-        set(gca,'units','centimeters')
-        pos = get(gca,'Position');
-        ti = get(gca,'TightInset');
-        set(gcf, 'PaperUnits','centimeters');
-        set(gcf, 'PaperSize', [pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
-        set(gcf, 'PaperPositionMode', 'manual');
-        set(gcf, 'PaperPosition',[0 0 pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
-    %Plot
-        plot(time_input, signal_input,'Color',[0.815 0.3294 0.3020])
-        hold on
-        plot(time_input,signal_output,'Color',[0.2157 0.4706 0.7490])
-        xlabel('Time (s)')
-        ylabel('Lateral Position (m)')
-        title('Lateral Perturbation Signal')
-        legend('Commanded','Measured')
-        xlim([100 200])
-    %Saving
-        h=figure(1);
-        saveas(h,'lateral_perturbation.pdf');
+
+% Settings to Improve Graph Appearance
+
+% TODO : I have no idea what this does and the figure doesn't come out to
+% an adequate size. I think it may just crop the image from the default
+% size which is incorrect.
+
+ti = get(gca, 'TightInset');
+set(gca, 'Position', [ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
+set(gca, 'units', 'centimeters')
+pos = get(gca, 'Position');
+ti = get(gca, 'TightInset');
+set(gcf, 'PaperUnits', 'centimeters');
+set(gcf, 'PaperSize',  [pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
+set(gcf, 'PaperPositionMode',  'manual');
+set(gcf, 'PaperPosition', [0 0 pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
+
+% Plot
+
+hold on
+
+plot(time_input, signal_input, 'Color', [0.815 0.3294 0.3020])
+plot(time_input, signal_output, 'Color', [0.2157 0.4706 0.7490])
+
+xlim([100 200])
+
+xlabel('Time (s)')
+ylabel('Lateral Position (m)')
+title('Lateral Perturbation Signal')
+legend('Commanded', 'Measured')
+
+% Saving
+
+h = figure(1);
+saveas(h, [PATHS.figures_dir filesep 'lateral_perturbation.pdf']);
