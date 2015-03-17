87c08ba6fda8f85b264ad2408beefb015aaadb9e
Jason K. Moore <moorepants@gmail.com>
2015-03-17 01:24:29 -0700

    Removed the commanded signal from the graph.

--- src/lateral_perturbation_plot.m
+++ src/lateral_perturbation_plot.m
@@ -10,25 +10,22 @@ clc
 
 PATHS = get_config();
 
-per_sig_dir = [PATHS.raw_data_dir filesep 'perturbation-signals'];
-lat_per_file = [per_sig_dir filesep 'lateral-perturbation.txt'];
 mocap_file = [PATHS.raw_data_dir filesep 'T006' filesep 'mocap-006.txt'];
 
 %-------------------------------------------------------------------------
 % Loading, Parsing, and Truncating
 %-------------------------------------------------------------------------
 
-perturbation_input = importdata(lat_per_file);
-frame = 1:1:length(perturbation_input);
-time_input = (frame - 1) * 0.0033;
-signal_input = perturbation_input(:, 2);
+start = 33090;
+stop = 57336;
 
 perturbation_output = importdata(mocap_file);
-time_output = perturbation_output.data(33090:57336, 1);
+
+time_output = perturbation_output.data(start:stop, 1);
 time_output = time_output - time_output(1, 1);
-signal_output = perturbation_output.data(33090:57336, 3);
+
+signal_output = perturbation_output.data(start:stop, 3);
 signal_output = signal_output - signal_output(1, 1);
-signal_output = interp1(time_output,signal_output,time_input);
 
 %-------------------------------------------------------------------------
 % Plotting
@@ -51,14 +48,12 @@ set(gcf, ...
 
 hold on
 
-plot(time_input, signal_input, 'Color', [0.815 0.3294 0.3020])
-plot(time_input, signal_output, 'Color', [0.2157 0.4706 0.7490])
+plot(time_output, signal_output, 'Color', [0.2157 0.4706 0.7490])
 
 xlim([100 200])
 
 xlabel('Time (s)')
 ylabel('Lateral Position (m)')
 title('Lateral Perturbation Signal')
-legend('Commanded', 'Measured')
 
 saveas(h, [PATHS.figures_dir filesep 'lateral_perturbation.pdf']);
